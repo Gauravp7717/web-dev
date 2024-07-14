a53390e9b777d1e0b940e1e73d9603e18cbe8d7d
Gauravp7717 <114347888+Gauravp7717@users.noreply.github.com>
2024-07-14 14:06:24 +0530

    changes in index html

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontSize:{
+        '3d': '3rem',
+      },
+    },
+  },
+  
+  plugins: [],
+}
+
